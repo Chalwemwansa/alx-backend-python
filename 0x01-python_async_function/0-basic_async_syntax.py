8e58de35d922c8e27ac8a65016672e11c8d27f66
chalwe mwansa <chalwemwansa23@gmail.com>
2024-04-23 02:54:58 +0200

    corrected logic error in task0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,9 +8,10 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """the function that delays for a given number of seconds then prints
     the float of the number of seconds it waits before printing a value"""
-    if max_delay < 0:
-        rand = random.uniform(0, max_delay - 1)
-    else:
+    if max_delay > 0:
         rand = random.uniform(0, max_delay + 1)
+    else:
+        max_delay = 0
+        rand = random.uniform(0, max_delay)
     await asyncio.sleep(rand)
     return (rand)
